cb13553d577b9aff4b9ae3a157984bf7caef8b31
Anais-Ek <anaiselisa77@gmail.com>
2022-10-12 16:33:20 +0200

    :building_construction: connected database with mongodb

--- app.js
+++ app.js
@@ -2,11 +2,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+mongoose.connect('mongodb+srv://pms:uwCnvM8JXTdftQJA@cluster0.fy9a1h1.mongodb.net/?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+  
+}).then(() =>{
+    console.log('Database Connected')
+}).catch(err=>{
+    console.log('Database not Connected'+err)
+});
 
 app.use(logger('dev'));
 app.use(express.json());
